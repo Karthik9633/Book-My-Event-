21c47a4e54249bb585042e42731902a749af4e18
Karthik B <karthikbalu5533@gmail.com>
2026-02-21 17:27:22 +0530

    a component for page navigations

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,34 @@
+const Pagination = () => {
+  return (
+    <div className="mt-12 flex flex-col items-center gap-6">
+
+      <button className="px-8 py-3 rounded-xl border font-semibold hover:bg-gray-100">
+        Load More Events
+      </button>
+
+      <div className="flex gap-3 items-center">
+        <span className="w-10 h-10 flex items-center justify-center bg-purple-600 text-white rounded-lg">
+          1
+        </span>
+        <span className="w-10 h-10 flex items-center justify-center hover:bg-gray-200 rounded-lg cursor-pointer">
+          2
+        </span>
+        <span className="w-10 h-10 flex items-center justify-center hover:bg-gray-200 rounded-lg cursor-pointer">
+          3
+        </span>
+         <span className="w-10 h-10 flex items-center justify-center hover:bg-gray-200 rounded-lg cursor-pointer">
+          4
+        </span>
+        <span className="w-10 h-10 flex items-center justify-center hover:bg-gray-200 rounded-lg cursor-pointer">
+          5
+        </span>
+        <span className="w-10 h-10 flex items-center justify-center hover:bg-gray-200 rounded-lg cursor-pointer">
+          ...
+        </span>
+      </div>
+
+    </div>
+  );
+};
+
+export default Pagination;
