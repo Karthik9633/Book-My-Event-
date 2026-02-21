f1a22a099275bdf0e97f366b9a7d23c49273d51b
Karthik B <karthikbalu5533@gmail.com>
2026-02-21 22:08:25 +0530

    Map page was fully udpated

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,11 +1,29 @@
+import { useState } from "react";
 import MapSidebar from "../components/MapSidebar";
 import MapView from "../components/MapView";
+import { events } from "../data/events";
 
 const MapPage = () => {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  const filteredEvents =
+    selectedCategory === "All"
+      ? events
+      : events.filter((e) => e.category === selectedCategory);
+
   return (
-    <div className="h-screen flex overflow-hidden bg-white">
-      <MapSidebar />
-      <MapView />
+    <div className="flex">
+
+      <MapSidebar
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+        events={events}
+      />
+
+      <div className="flex-1">
+        <MapView events={filteredEvents} />
+      </div>
+
     </div>
   );
 };
