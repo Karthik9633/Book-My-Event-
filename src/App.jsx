fd8cea52c73725a301e05bdb649c84490efa15aa
Karthik B <karthikbalu5533@gmail.com>
2026-02-21 17:29:50 +0530

    Everything was updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import AppRoutes from "./routes/AppRoutes";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Home from "./pages/Home";
+import Discover from "./pages/Discover";
+import EventDetails from "./pages/EventDetails";
+import CalendarPage from "./pages/CalendarPage";
+import MapPage from "./pages/MapPage";
+import SearchResults from "./pages/SearchResults";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/event/:id" element={<EventDetails />} />
+        <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/map" element={<MapPage />} />
+        <Route path="/search" element={<SearchResults />} />
+      </Routes>
+    </Layout>
+  );
 }
+
 
 export default App;
