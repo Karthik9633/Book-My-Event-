52cced9e92296b5035cea00c9a66297331d48831
Karthik B <karthikbalu5533@gmail.com>
2026-02-21 14:48:04 +0530

    A dummy map page was done

---
+++ src/pages/MapPage.jsx
@@ -0,0 +1,16 @@
+const MapPage = () => {
+  return (
+    <div className="flex">
+      <div className="w-1/3 p-6 bg-white shadow-md">
+        <h2 className="text-2xl font-bold mb-4">Events Nearby</h2>
+        <p>Event list here...</p>
+      </div>
+
+      <div className="w-2/3 h-screen bg-gray-300 flex items-center justify-center">
+        Map Placeholder
+      </div>
+    </div>
+  )
+}
+
+export default MapPage
