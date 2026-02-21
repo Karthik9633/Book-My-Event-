258c012efab1598ba91e3fafecf9f4daf9568e28
Karthik B <karthikbalu5533@gmail.com>
2026-02-21 20:33:15 +0530

    call mapsidebar and mapview and export MapPage

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -1,2 +1,13 @@
 import MapSidebar from "../components/MapSidebar";
 import MapView from "../components/MapView";
+
+const MapPage = () => {
+  return (
+    <div className="h-screen flex overflow-hidden bg-white">
+      <MapSidebar />
+      <MapView />
+    </div>
+  );
+};
+
+export default MapPage;
