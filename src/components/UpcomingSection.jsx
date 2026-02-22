36353e2973b59283e584eee80a57a8ef88c50a6a
Karthik B <karthikbalu5533@gmail.com>
2026-02-22 22:49:35 +0530

    responsive was done

--- src/components/UpcomingSection.jsx
+++ src/components/UpcomingSection.jsx
@@ -55,7 +55,7 @@ const UpcomingSection = () => {
       </div>
 
       {/* Events Grid */}
-      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {events.slice(startIndex, startIndex + itemsPerPage).map((event) => (
           <EventCard key={event.id} event={event} />
         ))}
