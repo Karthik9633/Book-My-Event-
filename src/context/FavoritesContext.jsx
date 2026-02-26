c7e70c94ef55e27d833e0749118f0cf2258aa793
Karthik B <karthikbalu5533@gmail.com>
2026-02-26 12:52:28 +0530

    Toast provider was addded

--- src/context/FavoritesContext.jsx
+++ src/context/FavoritesContext.jsx
@@ -1,23 +1,31 @@
 import { createContext, useContext, useState } from "react";
+import { useToast } from "./ToastContext";
 
 const FavoritesContext = createContext();
 
 export const FavoritesProvider = ({ children }) => {
   const [favorites, setFavorites] = useState([]);
+  const { showToast } = useToast();
 
   const toggleFavorite = (event) => {
     setFavorites((prev) => {
       const exists = prev.find((e) => e.id === event.id);
+
       if (exists) {
+        showToast("Removed from favourites");
         return prev.filter((e) => e.id !== event.id);
       } else {
+        showToast("Added to favourites");
         return [...prev, event];
       }
     });
   };
 
   const removeFavorite = (id) => {
-    setFavorites((prev) => prev.filter((event) => event.id !== id));
+    setFavorites((prev) => {
+      showToast("Removed from favourites");
+      return prev.filter((e) => e.id !== id);
+    });
   };
 
   const isFavorite = (id) => {
@@ -30,7 +38,7 @@ export const FavoritesProvider = ({ children }) => {
         favorites,
         toggleFavorite,
         removeFavorite,
-        isFavorite
+        isFavorite,
       }}
     >
       {children}
